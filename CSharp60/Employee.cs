432c6084d76d6758a54b0c86761640c320fe82c7
Rob Prouse <rob@prouse.org>
2014-09-08 12:14:53 -0400

    Initial commit of the project. Employee is now in pre-6.0 state.

---
+++ CSharp60/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharp60
+{
+    /// <summary>
+    /// An example of using primary constructors.
+    /// </summary>
+    public class Employee
+    {
+        private string _name;
+        private string _department;
+        private decimal _salary;
+
+        public Employee(string name, string department, decimal salary)
+        {
+            _name = name;
+            _department = department;
+            _salary = salary;
+        }
+
+        public string Name { get { return _name; } }
+        public string Department { get { return _department; } }
+        public decimal Salary { get { return _salary; } }
+    }
+}
---
+++ CSharp60/Tests/EmployeeTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CSharp60.Tests
+{
+    public class EmployeeTest
+    {
+        [Test]
+        public void TestConstructor()
+        {
+            var employee = new Employee("Rob Prouse", "Development", 12000);
+            Assert.That(employee.Name, Is.EqualTo("Rob Prouse"));
+            Assert.That(employee.Department, Is.EqualTo("Development"));
+            Assert.That(employee.Salary, Is.EqualTo(12000));
+        }
+    }
+}
