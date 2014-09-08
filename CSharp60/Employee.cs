3e2946ea27496cf998f2222b8b3f61bb3f01cc07
Rob Prouse <rob@prouse.org>
2014-09-08 12:26:17 -0400

    Converted the Employee class to use a Primary Constructor

--- CSharp60/Employee.cs
+++ CSharp60/Employee.cs
@@ -3,21 +3,10 @@
     /// <summary>
     /// An example of using primary constructors.
     /// </summary>
-    public class Employee
+    public class Employee(string name, string department, decimal salary)
     {
-        private string _name;
-        private string _department;
-        private decimal _salary;
-
-        public Employee(string name, string department, decimal salary)
-        {
-            _name = name;
-            _department = department;
-            _salary = salary;
-        }
-
-        public string Name { get { return _name; } }
-        public string Department { get { return _department; } }
-        public decimal Salary { get { return _salary; } }
+        public string Name { get; } = name;
+        public string Department { get; } = department;
+        public decimal Salary { get; } = salary;
     }
 }
